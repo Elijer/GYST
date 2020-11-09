fe1d9ef24e53c1f5436de47c8a9a23c4b73825db
Jah <kennedye2@vcu.edu>
2020-11-08 22:15:15 -0500

    fixed true diagonal bug

--- dist/GYST.e31bb0bc.js
+++ dist/GYST.e31bb0bc.js
@@ -142,22 +142,26 @@ var movePiece = function movePiece(pieceRow, pieceCol, destRow, destCol) {
         if (Math.abs(verticalRange) > 2 || Math.abs(horizontalRange) > 2) {
           console.log("That destination is out of range");
         } else {
-          if (Math.abs(horizontalRange) <= 1 || Math.abs(verticalRange) <= 1) {
+          if (Math.abs(horizontalRange) <= 1 && Math.abs(verticalRange) <= 1) {
             console.log("one square away. You can move here!");
             board[destRow][destCol] = position;
             board[pieceRow][pieceCol] = E;
           } else {
             var h = horizontalRange / 2;
             var v = verticalRange / 2;
-            var intermediateSquare = board[pieceRow + v][pieceCol + h];
-            console.log(intermediateSquare);
 
-            if (intermediateSquare != E) {
-              console.log("You may move here!");
-              board[destRow][destCol] = position;
-              board[pieceRow][pieceCol] = E;
+            if (h % 1 != 0 || v % 1 != 0) {
+              console.log("you can only skip other pieces on a true diagonal");
             } else {
-              console.log("You cannot move here because the square in between is empty.");
+              var intermediateSquare = board[pieceRow + v][pieceCol + h];
+
+              if (intermediateSquare != E) {
+                console.log("Two squares away. You may move here! (skip)");
+                board[destRow][destCol] = position;
+                board[pieceRow][pieceCol] = E;
+              } else {
+                console.log("You cannot move here because the square in between is empty.");
+              }
             }
           }
         }
@@ -170,8 +174,6 @@ var movePiece = function movePiece(pieceRow, pieceCol, destRow, destCol) {
   } else {
     console.log("piece is not defined");
   }
-
-  console.log(board);
 }; //movePiece(2, 2, 0, 0);
 
 
