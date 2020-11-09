147df0bfbc73a5a4cd379726d12566557e7b0c6d
Jah <kennedye2@vcu.edu>
2020-11-08 21:54:09 -0500

    made movePiece a bit more concise

--- dist/GYST.e31bb0bc.js
+++ dist/GYST.e31bb0bc.js
@@ -137,31 +137,26 @@ var movePiece = function movePiece(pieceRow, pieceCol, destRow, destCol) {
     if (position === player1.color || position === N) {
       if (destination === E) {
         var verticalRange = destRow - pieceRow;
+        var horizontalRange = destCol - pieceCol;
 
-        if (Math.abs(verticalRange) > 2) {
+        if (Math.abs(verticalRange) > 2 || Math.abs(horizontalRange) > 2) {
           console.log("That destination is out of range");
         } else {
-          var horizontalRange = destCol - pieceCol;
-
-          if (Math.abs(horizontalRange) > 2) {
-            console.log("That destination is out of range");
+          if (Math.abs(horizontalRange) <= 1 || Math.abs(verticalRange) <= 1) {
+            console.log("one square away. You can move here!");
+            board[destRow][destCol] = position;
+            board[pieceRow][pieceCol] = E;
           } else {
-            if (Math.abs(horizontalRange) <= 1 || Math.abs(verticalRange) <= 1) {
-              console.log("one square away. You can move here!");
+            var h = horizontalRange / 2;
+            var v = verticalRange / 2;
+            var intermediateSquare = board[pieceRow + h][pieceCol + v];
+
+            if (intermediateSquare != E) {
+              console.log("You may move here!");
               board[destRow][destCol] = position;
               board[pieceRow][pieceCol] = E;
             } else {
-              var h = horizontalRange / 2;
-              var v = verticalRange / 2;
-              var intermediateSquare = board[pieceRow + h][pieceCol + v];
-
-              if (intermediateSquare === B || intermediateSquare === N || intermediateSquare === W) {
-                console.log("You may move here!");
-                board[destRow][destCol] = position;
-                board[pieceRow][pieceCol] = E;
-              } else {
-                console.log("You cannot move here because the square in between is empty.");
-              }
+              console.log("You cannot move here because the square in between is empty.");
             }
           }
         }
@@ -192,13 +187,11 @@ var renderBoard = function renderBoard() {
         item.style.background = "rgb(80, 82, 98)";
 
         if (!selection) {
+          console.log(board);
           selection = [row, col];
-          console.log(selection);
         } else {
-          console.log(selection[0], selection[1], row, col);
           movePiece(selection[0], selection[1], row, col);
           selection = null;
-          console.log(board);
           document.getElementById("grid-container").innerHTML = "";
           renderBoard();
         }
--- index.js
+++ index.js
@@ -32,41 +32,34 @@ var movePiece = function(pieceRow, pieceCol, destRow, destCol){
     	if (destination === E){
       
         var verticalRange = destRow - pieceRow;
+        var horizontalRange = destCol - pieceCol;
 
-        if (Math.abs(verticalRange) > 2){
+        if (Math.abs(verticalRange) > 2 || Math.abs(horizontalRange) > 2){
+
           console.log("That destination is out of range");
+
         } else {
 
-          var horizontalRange = destCol - pieceCol;
-
-          if (Math.abs(horizontalRange) > 2){
-
-            console.log("That destination is out of range");
+          if (Math.abs(horizontalRange) <= 1 || Math.abs(verticalRange) <= 1){
+            console.log("one square away. You can move here!");
+              board[destRow][destCol] = position
+              board[pieceRow][pieceCol] = E;
 
           } else {
+          
+            var h = horizontalRange / 2;
+            var v = verticalRange / 2;
+            
+            var intermediateSquare = board[pieceRow + h][pieceCol + v];
 
-            if (Math.abs(horizontalRange) <= 1 || Math.abs(verticalRange) <= 1){
-              console.log("one square away. You can move here!");
-                board[destRow][destCol] = position
-                board[pieceRow][pieceCol] = E;
-
+            if (intermediateSquare != E ){  
+              console.log("You may move here!");
+              board[destRow][destCol] = position
+              board[pieceRow][pieceCol] = E;
             } else {
-            
-            	var h = horizontalRange / 2;
-              var v = verticalRange / 2;
-              
-              var intermediateSquare = board[pieceRow + h][pieceCol + v];
-
-              if (intermediateSquare === B || intermediateSquare === N || intermediateSquare === W){  
-              	console.log("You may move here!");
-                board[destRow][destCol] = position
-                board[pieceRow][pieceCol] = E;
-              } else {
-              	console.log("You cannot move here because the square in between is empty.")
-              }
-
+              console.log("You cannot move here because the square in between is empty.")
             }
-            
+
           }
           
         }
@@ -105,13 +98,12 @@ var renderBoard = function(){
         item.style.background = "rgb(80, 82, 98)";
 
         if (!selection){
+          console.log(board);
           selection = [row, col];
-          console.log(selection)
         } else {
-          console.log(selection[0], selection[1], row, col);
+
           movePiece(selection[0], selection[1], row, col);
           selection = null;
-          console.log(board);
           document.getElementById("grid-container").innerHTML = "";
           renderBoard();
 
