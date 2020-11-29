8da2cbe651b2e67ec9d2f13d0583dc867c3d788f
Jah <kennedye2@vcu.edu>
2020-11-29 10:44:52 -0500

    included firestore npm modules and consone.logged firestore

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/functions';
+
 import { gameplay } from "./gameplay";
+import { firebaseConfig } from "./firebaseConfig";
+
+document.addEventListener("DOMContentLoaded", event => {
+
+    firebase.initializeApp(firebaseConfig);
+
+    var db = firebase.firestore();
+    console.log(db);
+
+})
+
+
 
 //gameplay();
