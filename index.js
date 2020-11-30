f33837a883b675268ae12b07bfe480b795c47681
Jah <kennedye2@vcu.edu>
2020-11-29 19:37:41 -0500

    added a findGame onclick event test:

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/functions';
+import { firebaseConfig } from "./firebaseConfig";
 
 import { gameplay } from "./gameplay";
-import { firebaseConfig } from "./firebaseConfig";
 
 document.addEventListener("DOMContentLoaded", event => {
 
     firebase.initializeApp(firebaseConfig);
-
     var db = firebase.firestore();
     console.log(db);
 
 })
 
+Window.findGame = function(){
+    console.log("find game");
+};
 
 
 //gameplay();
